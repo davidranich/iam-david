a9a22bb67a6a06a2dabedf7f93322413fdfe5e34
David Ranich <david@Davids-MacBook-Pro.local>
2022-05-15 21:55:18 -0400

    A whole plethora of things. Added easter eggs, various bug fixes, and so on.

---
+++ src/lib/eth-price-scrape.js
@@ -0,0 +1,29 @@
+// install puppeteer
+// scrape some crypto website for ethereum price
+// save to json file
+
+// in js file, fetch json value 
+// find formula to convert ethereum to usd
+// set tooltip value
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+     const browser = await puppeteer.launch({ headless: true });
+     const page = await browser.newPage();
+     await page.goto('https://coinmarketcap.com/currencies/ethereum/');
+     const html = await page.$eval('.priceValue>span', (e) => e.innerHTML);
+     
+     const content = {
+          "price": html
+     };
+
+     fs.writeFile('./eth-price.json', JSON.stringify(content), err => {
+          if (err) {
+               console.error(err);
+          }
+          console.log('Success!');
+     });  
+     await browser.close();
+})();
