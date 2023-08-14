dba04ee7f404e9239f76fe92ec9611ea07d2c674
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-08-13 20:27:02 -0400

    Adjusted darkmode toggle

--- src/js/scripts/darkmode.js
+++ src/js/scripts/darkmode.js
@@ -7,7 +7,7 @@ if (!localStorage.getItem('dark-mode')) {
 const darkmode_val = localStorage.getItem('dark-mode');
 
 switch (darkmode_val) {
-     case '0':
+     case '1':
           light_dark_elm.classList.remove('fa-moon-stars');
           light_dark_elm.classList.add('fa-sun');
           document.documentElement.classList.remove('dark');
@@ -15,7 +15,7 @@ switch (darkmode_val) {
           document.body.classList.add('lightmode-bg');
           
           break;
-     case '1':
+     case '0':
           light_dark_elm.classList.add('fa-moon-stars');
           light_dark_elm.classList.remove('fa-sun');
           document.documentElement.classList.add('dark');
@@ -28,7 +28,7 @@ const DarkModeToggle = () => {
      const darkmode = localStorage.getItem('dark-mode');
 
      switch (darkmode) {
-          case '0':
+          case '1':
                light_dark_elm.classList.add('fa-moon-stars');
                light_dark_elm.classList.remove('fa-sun');
                localStorage.setItem('dark-mode', '1');
@@ -36,7 +36,7 @@ const DarkModeToggle = () => {
                document.body.classList.add('darkmode-bg');
                document.body.classList.remove('lightmode-bg');
                break;
-          case '1':
+          case '0':
                light_dark_elm.classList.remove('fa-moon-stars');
                light_dark_elm.classList.add('fa-sun');
                localStorage.setItem('dark-mode', '0');
