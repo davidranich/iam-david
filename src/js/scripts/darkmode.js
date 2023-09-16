19ac621a1def446e1798649ffd853402289c911e
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-09-16 01:26:27 -0400

    Refactored and fixed dark mode functionality

--- src/js/scripts/darkmode.js
+++ src/js/scripts/darkmode.js
@@ -1,51 +1,49 @@
 const light_dark_elm = document.getElementById("light-dark-mode");
 
 if (!localStorage.getItem('dark-mode')) { 
-     localStorage.setItem('dark-mode', '0');
+     localStorage.setItem('dark-mode', 0);
 }
 
-const darkmode_val = localStorage.getItem('dark-mode');
+let darkmode_val = localStorage.getItem('dark-mode');
+darkmode_val = parseInt(darkmode_val);
 
-switch (darkmode_val) {
-     case '0':
-          light_dark_elm.classList.remove('fa-moon-stars');
-          light_dark_elm.classList.add('fa-sun');
-          localStorage.setItem('dark-mode', '0');
+if (!darkmode_val) {
+     document.body.classList.add('lightmode-bg');
+     light_dark_elm.classList.add('fa-sun');
+} else {
+     document.documentElement.classList.add('dark');
+     document.body.classList.add('darkmode-bg');
+     light_dark_elm.classList.add('fa-moon-stars');
+}
+
+function toggle(value) {
+     value = parseInt(value);
+     if (!value) {
           document.documentElement.classList.remove('dark');
           document.body.classList.remove('darkmode-bg');
           document.body.classList.add('lightmode-bg');
-          break;
-     case '1':
-          light_dark_elm.classList.add('fa-moon-stars');
-          light_dark_elm.classList.remove('fa-sun');
-          localStorage.setItem('dark-mode', '1');
+     
+          light_dark_elm.classList.remove('fa-moon-stars');
+          light_dark_elm.classList.add('fa-sun');
+     } else {
           document.documentElement.classList.add('dark');
           document.body.classList.add('darkmode-bg');
           document.body.classList.remove('lightmode-bg');
-          break;
+     
+          light_dark_elm.classList.add('fa-moon-stars');
+          light_dark_elm.classList.remove('fa-sun');
+     }
 }
 
-const DarkModeToggle = () => {
-     const darkmode = localStorage.getItem('dark-mode');
+light_dark_elm.addEventListener("click", () => {
+     let darkmode = localStorage.getItem('dark-mode');
+     darkmode = parseInt(darkmode);
 
-     switch (darkmode) {
-          case '0':
-               light_dark_elm.classList.remove('fa-moon-stars');
-               light_dark_elm.classList.add('fa-sun');
-               localStorage.setItem('dark-mode', '1');
-               document.documentElement.classList.remove('dark');
-               document.body.classList.remove('darkmode-bg');
-               document.body.classList.add('lightmode-bg');
-               break;
-          case '1':
-               light_dark_elm.classList.add('fa-moon-stars');
-               light_dark_elm.classList.remove('fa-sun');
-               localStorage.setItem('dark-mode', '0');
-               document.documentElement.classList.add('dark');
-               document.body.classList.add('darkmode-bg');
-               document.body.classList.remove('lightmode-bg');
-               break;
+     if (!darkmode) {
+          toggle(1);
+          localStorage.setItem('dark-mode', 1);
+     } else {
+          toggle(0);
+          localStorage.setItem('dark-mode', 0);
      }
-};
-
-light_dark_elm.addEventListener("click", DarkModeToggle);
+});
