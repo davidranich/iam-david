cef216ab5d63dc77649a78d20ee513968a7271da
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-09-16 20:43:27 -0400

    Refactored like functionality

--- src/js/scripts/like-nft.js
+++ src/js/scripts/like-nft.js
@@ -1,42 +1,50 @@
+// grab heart favorite icon and count elements
 const favorite_elm = document.getElementById("favorite");
 const favorite_count = document.getElementById("fav-count");
 
+// if local storage item doesn't exist, let's create it
 if (!localStorage.getItem('nft-like')) { 
-     localStorage.setItem('nft-like', '0');
+     localStorage.setItem('nft-like', 0);
 }
 
-const like_val = localStorage.getItem('nft-like');
+// grab current local storage item value
+let like_val = parseInt(localStorage.getItem('nft-like'));
 
-switch (like_val) {
-     case '0':
-          favorite_elm.classList.remove('fas');
-          favorite_elm.classList.add('far');
-          favorite_count.innerHTML = like_val;
-          break;
-     case '1':
+// if not favorited, show outlined style icon; else show filled-in style icon
+// also set count based on 'nft-like' value
+if (!like_val) {
+     favorite_elm.classList.add('far');
+     favorite_count.innerHTML = like_val;
+} else {
+     favorite_elm.classList.add('fas');
+     favorite_count.innerHTML = like_val;
+}
+
+function toggle(value) {
+     // convert value to an integer
+     value = parseInt(value);
+
+     // if not favorited, show outlined style icon; else show filled-in style icon
+     if (!value) {
           favorite_elm.classList.add('fas');
           favorite_elm.classList.remove('far');
-          favorite_count.innerHTML = like_val;
-          break;
+          favorite_count.innerHTML = 1;
+          localStorage.setItem('nft-like', 1);
+     } else {
+          favorite_elm.classList.remove('fas');
+          favorite_elm.classList.add('far');
+          favorite_count.innerHTML = 0;
+          localStorage.setItem('nft-like', 0);
+     }
 }
 
-const FavoriteToggle = () => {
-     const like = localStorage.getItem('nft-like');
+favorite_elm.addEventListener("click", () => {
+     let like = parseInt(localStorage.getItem('nft-like'));
+     toggle(like);
 
-     switch (like) {
-          case '0':
-               favorite_elm.classList.add('fas');
-               favorite_elm.classList.remove('far');
-               localStorage.setItem('nft-like', '1');
-               favorite_count.innerHTML = "1";
-               break;
-          case '1':
-               favorite_elm.classList.remove('fas');
-               favorite_elm.classList.add('far');
-               localStorage.setItem('nft-like', '0');
-               favorite_count.innerHTML = "0";
-               break;
-     }
-};
-
-favorite_elm.addEventListener("click", FavoriteToggle);
+     // if (!like) {
+     //      toggle(1);
+     // } else {
+     //      toggle(0);
+     // }
+});
