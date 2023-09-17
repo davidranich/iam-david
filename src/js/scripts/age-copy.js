4726f6d612f8de3fde80c48d681d417857f61eac
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-09-16 21:05:10 -0400

    Little refactor of age and copyright generation

--- src/js/scripts/age-copy.js
+++ src/js/scripts/age-copy.js
@@ -1,9 +1,10 @@
-const date = new Date();
-const year = date.getFullYear();
-const birth_year = "1993";
+const current_year = new Date().getFullYear();
+const birth_year = 1993;
+let age = (current_year - birth_year); // simple math to determine age for 'about' page
 
+// age and copyright year elements
 const my_age = document.getElementById("my-age");
 const the_year = document.getElementById("copyright-year");
 
-my_age.innerHTML = year - birth_year;
-the_year.innerHTML = year;
+my_age.innerHTML = age;
+the_year.innerHTML = current_year;
