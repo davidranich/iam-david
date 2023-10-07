e3f8ab05121343f3a821b7139de731ff76ee6f75
David Ranich <david-mac-mini@Davids-Mac-mini.local>
2023-10-06 23:43:26 -0400

    Refactored dark/lightmode functionality

--- src/js/scripts/darkmode.js
+++ src/js/scripts/darkmode.js
@@ -1,6 +1,7 @@
 // light/dark element above copyright
 const light_dark_elm = document.getElementById("light-dark-mode");
 
+
 // if local storage item doesn't exist, let's create it
 if (!localStorage.getItem('dark-mode')) { 
      localStorage.setItem('dark-mode', 0);
@@ -9,46 +10,45 @@ if (!localStorage.getItem('dark-mode')) {
 // grab current local storage item value
 let darkmode_val = parseInt(localStorage.getItem('dark-mode'));
 
-// if dark-mode is not enabled (0), then deliver light mode; else, deliver dark mode
-if (!darkmode_val) {
-     document.body.classList.add('lightmode-bg');
-     light_dark_elm.classList.add('fa-sun');
-} else {
-     document.documentElement.classList.add('dark');
-     document.body.classList.add('darkmode-bg');
-     light_dark_elm.classList.add('fa-moon-stars');
+// if 'dark-mode' is not enabled (0), then deliver light mode; else, deliver dark mode
+setColorScheme(darkmode_val);
+
+function resetClasses() {
+     // reset bg classes
+     document.body.classList.remove('lightmode-bg');
+     document.documentElement.classList.remove('dark');
+     document.body.classList.remove('darkmode-bg');
+
+     // reset light/dark element icon
+     light_dark_elm.classList.remove('fa-sun');
+     light_dark_elm.classList.remove('fa-moon-stars');
 }
 
-function toggle(value) {
+function setColorScheme(value) {
+     // reset light/dark element icon
+     resetClasses();
+
      // convert value to an integer
      value = parseInt(value);
 
-     // if dark mode is enabled currently, disable it; else enable dark mode
-     if (!value) {
-          document.documentElement.classList.remove('dark');
-          document.body.classList.remove('darkmode-bg');
+     if (!value) { // light mode
           document.body.classList.add('lightmode-bg');
-     
-          light_dark_elm.classList.remove('fa-moon-stars');
-          light_dark_elm.classList.add('fa-sun');
-     } else {
+          light_dark_elm.classList.add('fa-moon-stars');
+     } else { // dark mode
           document.documentElement.classList.add('dark');
           document.body.classList.add('darkmode-bg');
-          document.body.classList.remove('lightmode-bg');
-     
-          light_dark_elm.classList.add('fa-moon-stars');
-          light_dark_elm.classList.remove('fa-sun');
+          light_dark_elm.classList.add('fa-sun');
      }
 }
 
 light_dark_elm.addEventListener("click", () => {
      let darkmode = parseInt(localStorage.getItem('dark-mode'));
 
-     if (!darkmode) {
-          toggle(1);
+     if (!darkmode) { // enable dark mode
+          setColorScheme(1);
           localStorage.setItem('dark-mode', 1);
-     } else {
-          toggle(0);
+     } else { // disable dark mode (enable light mode)
+          setColorScheme(0);
           localStorage.setItem('dark-mode', 0);
      }
 });
